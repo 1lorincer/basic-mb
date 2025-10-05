05cdf69386a7b369f3244416ad8e1bc314a743f9
asianParadissseee <asian.paradissseee@gmail.com>
2025-10-05 20:39:12 +0500

    checking widgets, styling and structure of classes

---
+++ android/app/src/main/kotlin/com/start/start_flutter_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.start.start_flutter_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:start_flutter_application/widgets/app.dart';
+
+void main() {
+  const app = App();
+  runApp(app);
+}
