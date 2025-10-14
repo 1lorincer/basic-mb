f4d1f1ceb315d0c527562cfe98f2e63aef8ba2f2
asianParadissseee <asian.paradissseee@gmail.com>
2025-10-14 21:09:44 +0500

    get another images

--- lib/design/images.dart
+++ lib/design/images.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_svg/flutter_svg.dart';
 
+// Vehicle images
 final SvgPicture vehicleMotorcycleImage = SvgPicture.asset(
   'assets/images/Motorcycle.svg',
 );
@@ -11,3 +12,19 @@ final SvgPicture vehicleTuktukImage = SvgPicture.asset(
 );
 final SvgPicture vehicleCarImage = SvgPicture.asset('assets/images/Car.svg');
 final SvgPicture vehicleBusImage = SvgPicture.asset('assets/images/Bus.svg');
+
+// State images
+final SvgPicture stateBoardingImage = SvgPicture.asset('assets/images/Boarding.svg');
+final SvgPicture stateDeliveryImage = SvgPicture.asset('assets/images/Delivery.svg');
+final SvgPicture stateLoadImage = SvgPicture.asset('assets/images/Load.svg');
+final SvgPicture stateParkingImage = SvgPicture.asset('assets/images/Parking.svg');
+final SvgPicture statePickupImage = SvgPicture.asset('assets/images/Pickup.svg');
+final SvgPicture stateRepairImage = SvgPicture.asset('assets/images/Repair.svg');
+final SvgPicture stateUnloadImage = SvgPicture.asset('assets/images/Unload.svg');
+final SvgPicture stateUnboardingImage = SvgPicture.asset('assets/images/unboarding.svg');
+
+// UI icons
+final SvgPicture accountCircleImage = SvgPicture.asset('assets/images/account_circle.svg');
+final SvgPicture arrowBackKiparoImage = SvgPicture.asset('assets/images/arrow_back kiparo.svg');
+final SvgPicture checkImage = SvgPicture.asset('assets/images/check.svg');
+
